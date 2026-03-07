beabd2dff4f238e3356a1e4722de5158a05ad828
brpuneet898 <brpuneet898@gmail.com>
2026-03-07 23:12:36 +0530

    updated data loader with comments

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -1,6 +1,9 @@
 import keras
 
 def load_dataset(dataset):
+    # this is the customizable function that will act as a dataloader. 
+    ## we have two main arguments, one is mnist and fashion mnist dataset. 
+    ## both of them correspond to classification task and that too multi-class classification problem.
     ds = None
     if dataset == "mnist":
         ds = keras.datasets.mnist.load_data(path="mnist.npz")
