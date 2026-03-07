089b0f3c6d2d462325872b295cb4f1a6f88ae52f
brpuneet898 <brpuneet898@gmail.com>
2026-03-07 23:11:18 +0530

    updated utils with comments

--- src/ann/utils.py
+++ src/ann/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def get_dead_neurons(a: np.ndarray):
+    # this is the helper function that will help to find out dead neurons of the neural
+    # network. It s based on a masking method and thresholding logic.
     alive_mask = np.any(a > 0, axis = 0)
     dead_indices = np.where(~alive_mask)[0]
     death_rate = len(dead_indices) / a.shape[1]
