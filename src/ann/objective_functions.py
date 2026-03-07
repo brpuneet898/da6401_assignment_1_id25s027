6c77a9f5b9d4f5db66cee1af0ee9336e14727044
brpuneet898 <brpuneet898@gmail.com>
2026-03-07 23:05:22 +0530

    updated objective function with comments

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -6,7 +6,15 @@ Implements: Cross-Entropy, Mean Squared Error (MSE)
 import numpy as np
 
 def cross_entropy(y: np.ndarray, y_pred: np.ndarray) -> float:
+    # this is the function that defines cross entropy loss function
+    # this will be particularly useful for the multi-class classification problems
+    # currently we will be using datasets like MNIST digits and MNIST fashion 
+    # they both are multiclass classfication task, so this will be highly useful
+    # this will use softmax activation function in the output layer 
+    # it will work on the logits and give out the probabilities.
     return -np.mean(np.sum(y * np.log(y_pred + 1e-8), axis = 1))
 
 def mse(y: np.ndarray, y_pred: np.ndarray) -> float:
+    # this is the function that defines MSE, this is particularly useful for the 
+    # regression problems.
     return np.mean((y - y_pred) ** 2)
